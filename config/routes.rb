4747dee9a5d07ec8a1f27bee813182105033383f
Alfonso Quintana <quintana.f.alfonso@gmail.com>
2019-02-14 15:52:24 -0300

    routes for cocktails added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resource :coktails  do
+    resource :cocktails, only: [:show, :index, :new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
